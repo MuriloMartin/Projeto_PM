f702b633da14c312adee2cb418f9a33552843964
luizafcamerini <luizacamerini@hotmail.com>
2023-06-25 18:57:29 -0300

    mais testes da user_interface e file_handler

--- tests.py
+++ tests.py
@@ -77,22 +77,22 @@ print("PRINTA VÁRIOS JOGOS DO DICIONARIO VAZIO:")
 list_games(games2)
 
 #teste de input apenas com letras
-print("CHECA INPUT INVÁLIDO (INPUT APENAS LETRA):")
+print("JOGO APENAS COM LETRAS:")
 check_game_input('hjk')
 print("\n")
 
 #teste de input com letra e numero
-print("CHECA INPUT INVÁLIDO (INPUT COM LETRA E NUMERO):")
+print("JOGO COM LETRA E NUMERO:")
 check_game_input('1l')
 print("\n")
 
 #teste de input vazio
-print("CHECA INPUT INVÁLIDO (INPUT VAZIO):")
+print("JOGO VAZIO:")
 check_game_input('')
 print("\n")
 
 #teste de input apenas com numeros
-print("CHECA INPUT VÁLIDO (INPUT APENAS NUMEROS):")
+print("JOGO VÁLIDO (APENAS COM NUMERO):")
 check_game_input('123')
 print("\n")
 
@@ -116,3 +116,122 @@ save_orders(lista_pedidos)
 create_request(0,lista_jogos)
 
 ################################ testes do user_interface.py ################################
+
+#pegando a ação do usuario
+get_user_action()
+print('\n')
+
+#pegando o cpf do usuario
+get_user_cpf()
+print('\n')
+
+#pegando o nome do usuario
+get_user_name()
+print('\n')
+
+#pegando o telefone do usuario
+get_user_phone()
+print('\n')
+
+#checando o cpf do usuario
+print("CPF VÁLIDO: (APENAS COM NUMEROS)")
+check_cpf('12548796760')
+print('\n')
+
+print("CPF VAZIO:")
+check_cpf('')
+print('\n')
+
+print("CPF COM LETRAS E NUMEROS:")
+check_cpf('125487A676a')
+print('\n')
+
+print("CPF APENAS COM LETRAS:")
+check_cpf('aaaaaaaaaaa')
+print('\n')
+
+print("CPF MENOR:")
+check_cpf('1a3456710')
+print('\n')
+
+print("CPF MAIOR:")
+check_cpf('123456789A011')
+print('\n')
+
+#checando o nome do usuario
+print("NOME VAZIO:")
+check_client_name('')
+print('\n')
+
+print("NOME APENAS COM NUMEROS:")
+check_client_name('1234567890')
+print('\n')
+
+print("NOME COM NUMEROS:")
+check_client_name('LUIZA123')
+print('\n')
+
+print("NOME COM CARACTERES ESPECIAIS:")
+check_client_name('LUIZA!!')
+print('\n')
+
+print("NOME COM LETRAS MINUSCULAS:")
+check_client_name('luiza')
+print('\n')
+
+print("NOME VÁLIDO:")
+check_client_name('LUIZA')
+print('\n')
+
+#checando o telefone do usuario
+print("TELEFONE VAZIO:")
+check_client_phone('')
+print('\n')
+
+print("TELEFONE APENAS COM LETRAS:")
+check_client_phone('bhjiujhgf')
+print('\n')
+
+print("TELEFONE COM CARACTERES ESPECIAIS:")
+check_client_phone('98796921!')
+print('\n')
+
+print("TELEFONE COM LETRAS E NUMEROS:")
+check_client_phone('98796921j0')
+print('\n')
+
+print("TELEFONE MAIOR:")
+check_client_phone('987969219000')
+print('\n')
+
+print("TELEFONE MENOR:")
+check_client_phone('98796921')
+print('\n')
+
+print("TELEFONE VÁLIDO:")
+check_client_phone('987969219')
+print('\n')
+
+#checando o tipo de aluguel
+print("TIPO DE ALUGUEL VAZIO:")
+check_rent_type('')
+print('\n')
+
+print("TIPO DE ALUGUEL APENAS COM LETRAS:")
+check_rent_type('bhjiujhgf')
+print('\n')
+
+print("TIPO DE ALUGUEL COM CARACTERES ESPECIAIS:")
+check_rent_type('98796921!')
+print('\n')
+
+print("TIPO DE ALUGUEL COM LETRAS E NUMEROS:")
+check_rent_type('98796921j0')
+print('\n')
+
+print("TIPO DE ALUGUEL VÁLIDO: (1)")
+check_rent_type('1')
+print('\n')
+
+print("TIPO DE ALUGUEL VÁLIDO: (2)")
+check_rent_type('2')
